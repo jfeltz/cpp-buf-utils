276229a2d5f35e13a249eb4bf0f0620168c8a897
John P. Feltz <jfeltz@gmail.com>
2014-09-26 19:36:17 -0600

    first population

---
+++ README.md
@@ -0,0 +1,73 @@
+cpp-buf-utils
+==============
+This Emacs library provides some basic methods for navigating and
+populating a c++ buffer with project dependencies. Emphasis is on
+configuring to the project at hand with just Elisp, rather than
+relying on **clang**, or some other c++ file inference engine.
+
+## Setup for Buffer Population
+The list, ```cpp-dependencies``` should be set on a per project basis,
+and *cpp-buf-utils* provides a set of small dsl-like functions
+for creating it. As an example: 
+```lisp
+(setq cpp-dependencies
+  (cpp-deps
+   (dep "iostream" (include-bkt "iostream"))
+   (dep "vector" (include-bkt "vector")
+   (dep "boost ns" nil (namespace "boost"))
+   (dep
+	 "boost program options"
+	 (include-path "boost/program_options.hpp")
+	 (namespaces
+	   (namespace-alias "boost::program_options" "po")))
+   (dep
+	 "boost spirit"
+	 (include-path "boost/spirit/include/qi.hpp")
+	 (namespaces
+	   (namespace-alias "boost::spirit::qi" "qi"))))))
+```
+
+*Legend*
+  * ```iostream```, and ```vector``` dep's define expansions for the STL *#include <iostream>* and *<vector>*
+  respectfully
+  * ```boost ns``` define a namespace expansion for a ```using namespace boost;``` expression.
+  * ```boost program options``` and ```boost spirit``` define both an #include, and namespace alias for *boost::program_options*
+
+As a side note, cpp-buf-deps also provides a list of common dependencies by default, see ```(std-cpp-deps)``` and ```(default-cpp-dependencies)```.
+
+# Buffer Operation 
+Then one can add members from this to the section by ido selection with:
+```
+M-x cpp-dep-add 
+```
+Which results in the minibuffer options:
+```
+add c++ dep: { iostream | vector | boost ns | boost program options | boost spirit } 
+```
+
+ido selecting ```iostream``` in this case results in ```#include <iostream>``` being included in
+the header section of the file.
+
+**A Note on how cpp-buf-utils searchers for addition points**
+
+cpp-buf-utils expects a *#include* section to already exist so it can find
+the insertion point, and similarly, a using declaration identifies the insertion
+point for namspaces. I'm considering adding a feature to use the cursor position
+when it is otherwise ambiguous. 
+
+# Buffer Navigation
+```elisp
+M-x c-include-visit
+```
+To jump to the last ```#include <..>``` definition. And:
+
+```elisp
+M-x c-include-unvisit
+```
+
+
+# Installation
+```lisp
+(add-to-list 'load-path "~/path/to/cpp-buf-utils/")
+(require 'cpp-buf-utils)
+```
---
+++ cpp-buf-utils.el
@@ -0,0 +1,207 @@
+;;; cpp-buf-utils.el --- navigation and ido expansions for common C++ namespace and include operations
+
+;; Copyright (C) 2014  John P. Feltz
+
+;; Author: John P. Feltz <jfeltz@gmail.com>
+;; Keywords: languages, convenience, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(defvar c-navigate-include-start-point nil)
+
+(defvar cpp-dependencies 
+  nil
+  (concat 
+    "The defined namespace/include depedencies for c++ projects.\n"
+    "See documentation on cpp-pragmatics for format.")
+  )
+
+(defun last-occurance (expr)
+  "return the last occurance without modifying the editor env"
+  (save-excursion
+    (goto-char (point-max))
+    (search-backward-regexp (concat "^" expr) (point-min) t)))
+
+(defun c-navigate-include ()
+  (setq c-navigate-include-start-point (point))
+  (let ((last (last-occurance "#include")))
+    (when last
+      (progn 
+        (goto-char last))))
+        (move-beginning-of-line nil))
+
+(defun c-navigate-include-return ()
+  "Return to the non-include point we were at before going to the include list."
+  (when c-navigate-include-start-point
+    (goto-char c-navigate-include-start-point)))
+
+(defun c-include-displacement (&optional away)
+  "visit or unvisit the include block based on arg" 
+  (if away (c-navigate-include-return) (c-navigate-include)))
+
+(defun c-include-visit ()
+  "visit c/c++ includes"
+  (interactive)
+  (c-include-displacement nil))
+
+(defun c-include-unvisit ()
+  (interactive)
+  "move away from the c/c++ includes to last known position, or same"
+  (c-include-displacement t)
+  )
+
+(defun cpp-dep-ns (namespace &optional alias)
+ "<namespace> <possible nil namespace alias>"
+ (values namespace alias)
+ )
+
+(defun dep (name header nl)
+ "args are: <fuzzy searchable str> <a list of header defs> <a list of namespace defs>"
+ (list name header nl))
+
+(defun cpp-dep-headers (&rest args) args)
+(defun cpp-deps (&rest args) args)
+(defun include (p) 
+  "takes a literal string to be included with #include str"
+  (values p)
+)
+
+(defun include-path (path)
+  "takes a path, and returns defintion for #include \"foobar.hpp\""
+  (include (concat "\"" path "\""))
+  )
+
+(defun include-bkt (kw)
+  "takes a keyword, and returns defintion for #include <foobarp>"
+  (include (concat "<" kw ">"))
+  )
+
+(defun namespaces (&rest args) args)
+(defun namespace (n) (list n nil))
+(defun namespace-alias (n a) (values n a))
+
+(defun diagnostic (msg) (message "%s" (concat "cpp-buf: " msg)))
+
+(defun insert-includes (includes)
+  "starting at current line pos, insert the include in defined list" 
+  ; pre-condition:
+  ;   cursor is at next line after include list 
+  (unless (not includes)  
+    (move-beginning-of-line nil)
+    (insert "\n") (forward-line -1) ; shove rest down one line 
+    (insert "#include " (car includes)) ; insertion
+    ; move down to put in pre-condition
+    (forward-line 1) 
+    (insert-includes (cdr includes))
+    )
+  )
+
+(defun namespace-insert (namespace-def)
+  "produce a namespace insert from the definition"
+  (let
+      ((n (car namespace-def)) (alias (car (cdr namespace-def))))
+  (if
+     (not alias)
+     (concat "using namespace " n ";")
+     (concat "namespace " alias " = " n ";")
+    )))
+
+(defun insert-namespaces (nl)
+  "starting at current line pos, insert the namespace defined" 
+  ; pre-condition:
+  ;   cursor is at next line after include list 
+  (unless (not nl)  
+    (move-beginning-of-line nil)
+    (insert "\n") (forward-line -1) ; shove rest down one line 
+    (insert (namespace-insert (car nl))) ; insertion
+    ; move down to put in pre-condition
+    (forward-line 1) 
+    (insert-namespaces (cdr nl))
+    )
+  )
+
+(defun update-block (f jobs sentinel name)
+  (let ((last (last-occurance sentinel)))
+    (if (not last) 
+        (diagnostic (concat "unable to locate " name " block"))
+        (progn 
+          (goto-char last)
+          (forward-line 1)
+          (move-beginning-of-line nil))
+          (funcall f jobs))))
+
+(defun update-includes (includes)
+  "Update buffer on given list of include files, e.g. (\"d.hpp\"
+\"a/b/c.hpp\"). If include block cannot be found to be updated, print
+failure"
+  (if includes (update-block 'insert-includes includes "#include" "include")))
+
+(defun update-namespaces (nl) 
+  "Update buffer on given list of namespace definitions. If namespace list
+block cannot be found to be updated, print failure"
+  (if nl (update-block 'insert-namespaces nl "using" "namespace")))
+
+(defun from-selection (k)
+  "manipulate the buffer from a dependency key selection" 
+  (progn 
+    (update-includes ; the list of include defs
+     (car (cdr (assoc k cpp-dependencies))))
+    (message (format "%s" (assoc k cpp-dependencies)))
+    (update-namespaces ; the list of namespace defs
+     (car (cdr (cdr (assoc k cpp-dependencies)))))))
+
+(defun to-ido-indexes (def)
+  "produce ido-indexes from the def"
+  (mapcar 'car cpp-dependencies))
+
+(defun cpp-dep-add ()
+  (interactive)
+  (save-excursion 
+    (from-selection
+     (ido-completing-read
+     "add c++ dep: "
+     (to-ido-indexes cpp-dependencies)))))
+
+(defun std-cpp-deps () 
+ (let 
+   ((includes 
+      '("iostream"
+        "sstream"
+        "vector"
+        "list"
+        "algorithm"
+        "set"
+        "map")))
+   (mapcar 'from-kw includes)))
+
+(defun from-kw (kw) (dep kw (include-bkt kw) ()))
+
+(defun default-cpp-dependencies ()
+  (append (std-cpp-deps)
+    (cpp-deps
+     (dep "boost ns" nil (namespace "boost"))
+     (dep
+       "boost program options"
+       (include-path "boost/program_options.hpp")
+       (namespaces
+         (namespace-alias "boost::program_options" "po")))
+     (dep
+       "boost spirit"
+       (include-path "boost/spirit/include/qi.hpp")
+       (namespaces
+         (namespace-alias "boost::spirit::qi" "qi"))))))
+
+(provide 'cpp-buf-utils)
